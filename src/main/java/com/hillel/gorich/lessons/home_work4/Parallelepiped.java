11a6bf4213f8c8c76d8ef6b3a329ed7146767dec
Gorich Ivan <ivan.kontakt@ukr.net>
2022-10-28 17:57:56 +0300

    Home_Work_4

---
+++ src/main/java/com/hillel/gorich/lessons/home_work4/Parallelepiped.java
@@ -0,0 +1,17 @@
+package com.hillel.gorich.lessons.home_work4;
+
+public class Parallelepiped {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 7;
+
+        int volume = a * b * c;
+        System.out.println("Parallelepiped volume = " + volume);
+        System.out.println();
+
+        int length = a * 4 + b * 4 + c * 4;
+        System.out.println("Total Length = " + length);
+
+    }
+}
