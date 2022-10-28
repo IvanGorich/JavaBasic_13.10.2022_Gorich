82c1db5eba017c6373956fb194e3af35ff0a9161
Gorich Ivan <ivan.kontakt@ukr.net>
2022-10-28 18:00:45 +0300

    Home_Work_5

---
+++ src/main/java/com/hillel/gorich/lessons/home_work4/ChineseDynasties.java
@@ -0,0 +1,26 @@
+package com.hillel.gorich.lessons.home_work4;
+
+public class ChineseDynasties {
+    public static void main(String[] args) {
+        int attackSoldierLi = 13;
+        int attackArcherLi = 24;
+        int attackRiderLi = 46;
+
+        int sumLi = 860;
+
+        int sumAmountArmyLi = (attackSoldierLi + attackArcherLi + attackRiderLi) * sumLi;
+
+        System.out.println("IndexSumAmountArmyLi = " + sumAmountArmyLi);
+
+
+        int attackSoldierMin = 9;
+        int attackArcherMin = 35;
+        int attackRiderMin = 12;
+
+        final double COEFFICIENT = sumLi * 1.5;
+
+        double sumAmountArmyMin = (attackSoldierMin + attackArcherMin + attackRiderMin) * COEFFICIENT;
+
+        System.out.println("IndexSumAmountArmyMin = " + sumAmountArmyMin);
+    }
+}
