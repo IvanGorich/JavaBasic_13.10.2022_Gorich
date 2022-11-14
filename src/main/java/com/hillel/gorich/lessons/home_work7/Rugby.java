0dc265eb22c5d252b254357408c642f05d2a35bd
Gorich Ivan <ivan.kontakt@ukr.net>
2022-11-14 11:34:22 +0200

    Rugby

---
+++ src/main/java/com/hillel/gorich/lessons/home_work7/Rugby.java
@@ -0,0 +1,71 @@
+package com.hillel.gorich.lessons.home_work7;
+
+import java.util.Arrays;
+
+public class Rugby {
+    public static void main(String[] args) {
+        int min = 18;
+        int max = 40;
+        int[] team1 = new int[25];
+        int[] team2 = new int[25];
+        int sumAgeTeam1 = 0;
+        int sumAgeTeam2 = 0;
+        double averageAgeTeam1 = 0;
+        double averageAgeTeam2 = 0;
+
+        for (int i = 0; i < team1.length; i++) {
+            team1[i] = (int) (Math.random() * (max - min + 1) + min);
+        }
+        for (int i = 0; i < team1.length; i++) {
+            if (i == 0) {
+                System.out.print("Age list team1: " + team1[i] + ", ");
+            }
+            if (i != 0 && i < team1.length) {
+                System.out.print(team1[i] + ", ");
+            }
+            if (i == team1.length - 1) {
+                System.out.println(team1[i] + ".");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < team2.length; i++) {
+            team2[i] = (int) (Math.random() * (max - min + 1) + min);
+        }
+        for (int i = 0; i < team2.length; i++) {
+            if (i == 0) {
+                System.out.print("Age list team2: " + team2[i] + ", ");
+            }
+            if (i != 0 && i < team2.length) {
+                System.out.print(team2[i] + ", ");
+            }
+            if (i == team2.length - 1) {
+                System.out.println(team2[i] + ".");
+            }
+        }
+
+        for (int i = 0; i < team1.length; i++) {
+            sumAgeTeam1 += team1[i];
+            averageAgeTeam1 = sumAgeTeam1 / team1.length;
+        }
+        System.out.println(sumAgeTeam1);
+        System.out.println("Average age of first team players: " + Math.round(averageAgeTeam1));
+        System.out.println();
+
+        for (int i = 0; i < team2.length; i++) {
+            sumAgeTeam2 += team2[i];
+            averageAgeTeam2 = sumAgeTeam2 / team2.length;
+        }
+        System.out.println(sumAgeTeam2);
+        System.out.println("Average age of second team players: " + Math.round(averageAgeTeam2));
+        System.out.println();
+
+        if (averageAgeTeam1 > averageAgeTeam2) {
+            System.out.println("Average age of first team players more by: " + (averageAgeTeam1 - averageAgeTeam2));
+        } else if (averageAgeTeam2 > averageAgeTeam1) {
+            System.out.println("Average age of second team players more by: " + (averageAgeTeam2 - averageAgeTeam1));
+        } else {
+            System.out.println("The average age of players on both teams is the same.");
+        }
+    }
+}
