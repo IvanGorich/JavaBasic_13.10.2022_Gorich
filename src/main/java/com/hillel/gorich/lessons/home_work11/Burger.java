c6c1594525d84d5a0ed04104f02ad4151a4ef9c9
Gorich Ivan <ivan.kontakt@ukr.net>
2022-12-06 17:59:10 +0200

    BurgersNewVariable

--- src/main/java/com/hillel/gorich/lessons/home_work11/Burger.java
+++ src/main/java/com/hillel/gorich/lessons/home_work11/Burger.java
@@ -3,10 +3,10 @@ package com.hillel.gorich.lessons.home_work11;
 public class Burger {
     String bun;
     String meat;
+    String plusMeat;
     String cheese;
     String greens;
     String mayo;
-    int n;
 
     public Burger(String bun, String meat, String cheese, String greens, String mayo) {
         this.bun = bun;
@@ -16,19 +16,41 @@ public class Burger {
         this.mayo = mayo;
     }
 
+    @Override
+    public String toString() {
+        return "Burger 'full' consists of:" +
+                " " + bun +
+                ", " + meat +
+                ", " + cheese +
+                ", " + greens +
+                ", " + mayo +
+                '.';
+    }
+
     public Burger(String bun, String meat, String cheese, String greens) {
         this.bun = bun;
         this.meat = meat;
         this.cheese = cheese;
         this.greens = greens;
+
+        printInfo();
     }
 
-    public Burger(String bun, String meat, int n, String cheese, String greens, String mayo) {
+    void printInfo() {
+        System.out.println("Burger 'dietetic' consists of: " + bun + ", " + meat
+                + ", " + cheese + ", " + greens + ".");
+    }
+
+
+    public Burger(String bun, String meat, String plusMeat, String cheese, String greens, String mayo) {
         this.bun = bun;
         this.meat = meat;
+        this.plusMeat = plusMeat;
         this.cheese = cheese;
         this.greens = greens;
         this.mayo = mayo;
-        this.n = 2;
+        System.out.println("Burger 'doubleMeat' consists of: " + bun + ", " + meat + ", " +
+                plusMeat + ", " + cheese + ", " + greens + ", " + mayo +
+                ".");
     }
 }
--- src/main/java/com/hillel/gorich/lessons/home_work11/BurgerMain.java
+++ src/main/java/com/hillel/gorich/lessons/home_work11/BurgerMain.java
@@ -3,19 +3,11 @@ package com.hillel.gorich.lessons.home_work11;
 public class BurgerMain {
     public static void main(String[] args) {
         Burger full = new Burger("bun", "meat", "cheese", "greens", "mayo");
+        System.out.println(full);
 
-        System.out.println("Burger 'full' consists of: " + full.bun + " + " + full.meat
-                + " + " + full.cheese + " + " + full.greens + " + " + full.mayo + ".");
+        Burger dietetic = new Burger("bun", "meat", "cheese", "greens");
 
-
-        Burger dietetic = new Burger(full.bun, full.meat, full.cheese, full.greens);
-
-        System.out.println("Burger 'dietetic' consists of: " + dietetic.bun + " + " + dietetic.meat
-                + " + " + dietetic.cheese + " + " + dietetic.greens + ".");
-
-        Burger doubleMeat = new Burger(full.bun, full.meat, full.n, full.cheese, full.greens, full.mayo);
-
-        System.out.println("Burger 'doubleMeat' consists of: " + doubleMeat.bun + " + " + doubleMeat.meat
-                + " * " + doubleMeat.n + " + " + doubleMeat.cheese + " + " + doubleMeat.greens + doubleMeat.mayo + ".");
+        Burger doubleMeat = new Burger("bun", "meat", "plusMeat", "cheese", "greens",
+                "mayo");
     }
 }
